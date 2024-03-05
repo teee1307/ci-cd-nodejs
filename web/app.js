abc3b3ff5a344d1353c7935b8b269b9e99ee4dc7
root <root@DESKTOP-2I1UI4J>
2024-03-05 16:43:01 +0530

    added some changes to routes

--- web/app.js
+++ web/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+var http = require('http');
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
@@ -21,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
-app.get('/tanaya').then(console.log("working!!"))
+http.get('/tanaya').then(console.log("working!!"))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
